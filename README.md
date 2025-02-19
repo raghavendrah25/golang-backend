73cd37a832953ad19c5bbcdbb7eb337c689e3fb2
raghavendrah25 <152840271+raghavendrah25@users.noreply.github.com>
2025-02-19 22:59:44 +0530

    Updated

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 This repository contains a Golang backend program for an e-commerce website.
 
+*-> I am using Jfrog for code scanning and vulnerability scanning during development phase. 
+
 ## Features
+
+*-> Using Go Gin for HTTP framework
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/Rhymond/go-money"
+	"github.com/gin-gonic/gin"
+	"github.com/raghavendrah25/golang-backend/internal/product"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/products", func(c *gin.Context) {
+		products := []product.Product{
+			{
+				ID:		  "1",
+				Name:        "Apple",
+				Description: "Apple iPhone 12",
+				PriceVATExcluded: money.New(1000, "USD"),
+				VAT:              money.New(200, "USD"),
+			},
+			{
+				ID:		  "2",
+				Name:        "Samsung",
+				Description: "Samsung Galaxy S21",
+				PriceVATExcluded: money.New(900, "USD"),
+				VAT:              money.New(180, "USD"),
+			},
+			{
+				ID:		  "3",
+				Name:        "OnePlus",
+				Description: "OnePlus 9 Pro",
+				PriceVATExcluded: money.New(800, "USD"),
+				VAT:              money.New(160, "USD"),
+			},
+		}
+		c.JSON(http.StatusOK, products)
+	})
+	r.Run()
 }
